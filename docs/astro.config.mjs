6817ccd7b7aa43dcd87263aaf3ce0a1c1aee5386
Christian Bergschneider <christian.bergschneider@gmx.de>
2026-08-04 01:08:58 +0200

    feat: add GitHub Actions workflows for build, release, and website deployment; update docs and CNAME configuration

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://twaddle.asorda.net',
 	integrations: [
 		starlight({
 			title: 'Tools & twaddle',
